1ef9f63c45b59fb5cc10c243e2e8ba74a576851f
Niravcodez <84302842+Niravcodez@users.noreply.github.com>
2021-11-18 17:48:26 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,48 @@ function modelLoaded()
 {
     console.log("Model Loaded!");
 }
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data1 = toSpeak;
+    var utterthis = new SpeechSynthesisUtterance(speak_data1);
+    synth.speak(utterthis);
+}
+
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        toSpeak = "";
+        gesture = results[0].label;
+        if(gesture == "amazing")
+        {
+            toSpeak = "This is Amazing!";
+            document.getElementById("udpate_emoji").innerHTML = "&#9996;";
+        }
+        else if(gesture == "best")
+        {
+            toSpeak = "All the Best!";
+            document.getElementById("udpate_emoji").innerHTML = "&#128077;";
+        }
+        else if(gesture == "victory")
+        {
+            toSpeak = "That was a Marvelous Victory";
+            document.getElementById("udpate_emoji").innerHTML = "&#128076;";
+        }
+        speak();
+    }
+}
